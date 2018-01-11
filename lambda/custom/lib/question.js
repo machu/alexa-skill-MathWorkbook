24fd16db928c61fedd2cc5df11ca2e7500eef7fb
MATSUOKA Kohei <kohei@machu.jp>
2018-01-12 03:38:06 +0900

    implement question logic

--- lambda/custom/lib/question.js
+++ lambda/custom/lib/question.js
@@ -19,7 +19,7 @@ levels[1] = () => {
 levels[2] = () => {
     const r = {};
     r.x = createRandomNumber(1, 9);
-    r.y = createRandomNumber(1, 9);
+    r.y = createRandomNumber(10 - r.x, 9);
     r.answer = r.x + r.y;
     return r;
 };
@@ -27,9 +27,27 @@ levels[2] = () => {
 // レベル3 … 2桁の足し算（答えが30以下）
 levels[3] = () => {
     const r = {};
-    r.x = createRandomNumber(1, 30);
-    r.answer = createRandomNumber(r.x, 30);
-    r.y = r.answer - r.x;
+    r.x = createRandomNumber(10, 20);
+    r.y = createRandomNumber(10, 30 - r.x);
+    r.answer = r.x + r.y;
+    return r;
+};
+
+// レベル4 … 2桁の足し算（答えが50以下）
+levels[4] = () => {
+    const r = {};
+    r.x = createRandomNumber(10, 40);
+    r.y = createRandomNumber(10, 50 - r.x);
+    r.answer = r.x + r.y;
+    return r;
+};
+
+// レベル5 … 2桁の足し算
+levels[5] = () => {
+    const r = {};
+    r.x = createRandomNumber(10, 99);
+    r.y = createRandomNumber(10, 99);
+    r.answer = r.x + r.y;
     return r;
 };
 
--- lambda/custom/test/question.test.js
+++ lambda/custom/test/question.test.js
@@ -7,8 +7,85 @@ describe ('Question', () => {
         context('when level1', () => {
             it('繰り上がりがないこと', () => {
                 for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion();
+                    const q = Question.createQuestion(1);
                     assert(q.answer <= 10);
+                }
+            });
+        });
+        context('when level2', () => {
+            it('xが10未満であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(2);
+                    assert(q.x < 10);
+                }
+            });
+            it('yが10未満であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(2);
+                    assert(q.y < 10);
+                }
+            });
+            it('繰り上がりの足し算であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(2);
+                    assert(q.answer >= 10);
+                    assert(q.answer < 20);
+                }
+            });
+        });
+        context('when level3', () => {
+            it('xが10以上であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(3);
+                    assert(q.x >= 10);
+                }
+            });
+            it('yが10以上であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(3);
+                    assert(q.y >= 10);
+                }
+            });
+            it('答えが30以下であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(3);
+                    assert(q.answer <= 30);
+                }
+            });
+        });
+        context('when level4', () => {
+            it('xが10以上であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(4);
+                    assert(q.x >= 10);
+                }
+            });
+            it('yが10以上であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(4);
+                    assert(q.y >= 10);
+                }
+            });
+            it('答えが50以下であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(4);
+                    assert(q.answer <= 50);
+                }
+            });
+        });
+        context('when level5', () => {
+            it('xが10以上100未満であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(5);
+                    assert(q.x >= 10);
+                    assert(q.x < 100);
+                }
+            });
+            it('yが10以上100未満であること', () => {
+                for (let i = 0; i < 100; i++) {
+                    const q = Question.createQuestion(5);
+                    assert(q.y >= 10);
+                    assert(q.y < 100);
                 }
             });
         });
