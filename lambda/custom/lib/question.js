45e8be47f61c1289a1812479b29f3b3adb3b1465
MATSUOKA Kohei <kohei@machu.jp>
2018-01-15 02:42:13 +0900

    implements subtraction

--- lambda/custom/lib/question.js
+++ lambda/custom/lib/question.js
@@ -1,6 +1,13 @@
 exports.createQuestion = (level = 1, operation = "addition") => {
-    const question = levels[level]();
-    question.speech = `${question.x}たす、${question.y}は？`;
+    const question = levels[operation][level]();
+    const operater_table = {
+        "addition": "たす",
+        "subtraction": "ひく",
+        "multiplication": "掛ける",
+        "division": "割る"
+    };
+    // 1たす、2は？
+    question.speech = `${question.x}${operater_table[operation]}、${question.y}は？`;
     return question;
 };
 
@@ -22,52 +29,132 @@ exports.getLevelFromSlots = (slots) => {
     };
 };
 
-const levels = {};
-
-// レベル1 … 1桁の足し算（繰り上がりなし）
-levels[1] = () => {
-    const r = {};
-    r.x = createRandomNumber(1, 9);
-    r.y = createRandomNumber(1, 10 - r.x);
-    r.answer = r.x + r.y;
-    return r;
+const levels = {
+    "addition": {
+        // レベル1 … 1桁の足し算（繰り上がりなし）
+        "1": () => {
+            const r = {};
+            while (!(r.answer <= 10)) {
+                r.x = createRandomNumber(1, 9);
+                r.y = createRandomNumber(1, 9);
+                r.answer = r.x + r.y;
+            }
+            return r;
+        },
+        // レベル2 … 1桁の足し算（繰り上がりあり）
+        "2": () => {
+            const r = {};
+            while (!(r.answer > 10)) {
+                r.x = createRandomNumber(1, 9);
+                r.y = createRandomNumber(1, 9);
+                r.answer = r.x + r.y;
+            }
+            return r;
+        },
+        // レベル3 … 2桁の足し算（答えが30以下）
+        "3": () => {
+            const r = {};
+            while (!(r.answer < 30)) {
+                r.x = createRandomNumber(10, 20);
+                r.y = createRandomNumber(10, 20);
+                r.answer = r.x + r.y;
+            }
+            return r;
+        },
+        // レベル4 … 2桁の足し算（答えが50以下）
+        "4": () => {
+            const r = {};
+            while (!(r.answer < 50)) {
+                r.x = createRandomNumber(10, 40);
+                r.y = createRandomNumber(10, 40);
+                r.answer = r.x + r.y;
+            }
+            return r;
+        },
+        // レベル5 … 2桁の足し算
+        "5": () => {
+            const r = {};
+            r.x = createRandomNumber(10, 99);
+            r.y = createRandomNumber(10, 99);
+            r.answer = r.x + r.y;
+            return r;
+        }
+    },
+    "subtraction": {
+        // レベル1 … 1桁の引き算（繰り下がりなし）
+        "1": () => {
+            const r = {};
+            nums = [createRandomNumber(1, 10), createRandomNumber(1, 10)];
+            r.x = Math.max(...nums);
+            r.y = Math.min(...nums);
+            r.answer = r.x - r.y;
+            return r;
+        },
+        // レベル2 … 2桁 - 1桁
+        "2": () => {
+            const r = {};
+            r.x = createRandomNumber(10, 19);
+            r.y = createRandomNumber(1, 9);
+            r.answer = r.x - r.y;
+            return r;
+        },
+        // レベル3 … 2桁 - 2桁（x < 30）
+        "3": () => {
+            const r = {};
+            nums = [createRandomNumber(11, 29), createRandomNumber(11, 29)];
+            r.x = Math.max(...nums);
+            r.y = Math.min(...nums);
+            r.answer = r.x - r.y;
+            return r;
+        },
+        // レベル4 … 2桁 - 2桁（x < 50）
+        "4": () => {
+            const r = {};
+            nums = [createRandomNumber(11, 49), createRandomNumber(11, 49)];
+            r.x = Math.max(...nums);
+            r.y = Math.min(...nums);
+            r.answer = r.x - r.y;
+            return r;
+        },
+        // レベル5 … 3桁 - 2桁
+        "5": () => {
+            const r = {};
+            r.x = createRandomNumber(100, 199);
+            r.y = createRandomNumber(11, 99);
+            r.answer = r.x - r.y;
+            return r;
+        }
+    },
+    "multiplication": {
+        "1": () => {
+        },
+        "2": () => {
+        },
+        "3": () => {
+        },
+        "4": () => {
+        },
+        "5": () => {
+        }
+    },
+    "division": {
+        "1": () => {
+        },
+        "2": () => {
+        },
+        "3": () => {
+        },
+        "4": () => {
+        },
+        "5": () => {
+        }
+    }
 };
 
-// レベル2 … 1桁の足し算（繰り上がりあり）
-levels[2] = () => {
-    const r = {};
-    r.x = createRandomNumber(1, 9);
-    r.y = createRandomNumber(10 - r.x, 9);
-    r.answer = r.x + r.y;
-    return r;
-};
 
-// レベル3 … 2桁の足し算（答えが30以下）
-levels[3] = () => {
-    const r = {};
-    r.x = createRandomNumber(10, 20);
-    r.y = createRandomNumber(10, 30 - r.x);
-    r.answer = r.x + r.y;
-    return r;
-};
 
-// レベル4 … 2桁の足し算（答えが50以下）
-levels[4] = () => {
-    const r = {};
-    r.x = createRandomNumber(10, 40);
-    r.y = createRandomNumber(10, 50 - r.x);
-    r.answer = r.x + r.y;
-    return r;
-};
 
-// レベル5 … 2桁の足し算
-levels[5] = () => {
-    const r = {};
-    r.x = createRandomNumber(10, 99);
-    r.y = createRandomNumber(10, 99);
-    r.answer = r.x + r.y;
-    return r;
-};
+
 
 const createRandomNumber = (min = 0, max = 100) => {
     return Math.floor(Math.random() * (max + 1 - min) + min);
--- lambda/custom/test/question.test.js
+++ lambda/custom/test/question.test.js
@@ -4,90 +4,123 @@ const Question = require('../lib/question');
 
 describe ('Question', () => {
     describe('#createQuestion()', () => {
-        context('when level1', () => {
-            it('繰り上がりがないこと', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(1);
-                    assert(q.answer <= 10);
-                }
+        context('addition', () => {
+            context('when level1', () => {
+                it('繰り上がりがないこと', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(1);
+                        assert(q.answer <= 10);
+                    }
+                });
+            });
+            context('when level2', () => {
+                it('1桁の足し算（繰り上がりあり）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(2);
+                        assert(q.x < 10);
+                        assert(q.y < 10);
+                        assert(q.answer > 10);
+                        assert(q.answer < 20);
+                    }
+                });
+            });
+            context('when level3', () => {
+                it('2桁の足し算（答えが30以下）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(3);
+                        assert(q.x >= 10);
+                        assert(q.y >= 10);
+                        assert(q.answer <= 30);
+                    }
+                });
+            });
+            context('when level4', () => {
+                it('2桁の足し算（答えが50以下）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(4);
+                        assert(q.x >= 10);
+                        assert(q.y >= 10);
+                        assert(q.answer <= 50);
+                    }
+                });
+            });
+            context('when level5', () => {
+                it('2桁の足し算（制限なし）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(5);
+                        assert(q.x >= 10);
+                        assert(q.x < 100);
+                        assert(q.y >= 10);
+                        assert(q.y < 100);
+                    }
+                });
             });
         });
-        context('when level2', () => {
-            it('xが10未満であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(2);
-                    assert(q.x < 10);
-                }
+        context('subtraction', () => {
+            context('when level1', () => {
+                it('繰り下がりがないこと', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(1, "subtraction");
+                        assert(q.x <= 10);
+                        assert(q.y <= 10);
+                        assert(q.answer >= 0);
+                        assert(q.answer < 10);
+                    }
+                });
             });
-            it('yが10未満であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(2);
-                    assert(q.y < 10);
-                }
+            context('when level2', () => {
+                it('2桁と1桁の引き算であること(x < 20)', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(2, "subtraction");
+                        assert(q.x >= 10);
+                        assert(q.x < 20);
+                        assert(q.y < 10);
+                        assert(q.answer >= 0);
+                    }
+                });
             });
-            it('繰り上がりの足し算であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(2);
-                    assert(q.answer >= 10);
-                    assert(q.answer < 20);
-                }
+            context('when level3', () => {
+                it('2桁同士の引き算であること（x < 30）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(3, "subtraction");
+                        assert(q.x > 10);
+                        assert(q.x < 30);
+                        assert(q.y > 10);
+                        assert(q.y < 30);
+                        assert(q.answer >= 0);
+                    }
+                });
+            });
+            context('when level4', () => {
+                it('2桁同士の引き算であること（x < 50）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(4, "subtraction");
+                        assert(q.x > 10);
+                        assert(q.x < 50);
+                        assert(q.y > 10);
+                        assert(q.y < 50);
+                        assert(q.answer >= 0);
+                        console.log(q);
+                    }
+                });
+            });
+            context('when level5', () => {
+                it('3桁と2桁の引き算であること（x > 200）', () => {
+                    for (let i = 0; i < 100; i++) {
+                        const q = Question.createQuestion(5, "subtraction");
+                        assert(q.x >= 100);
+                        assert(q.x < 200);
+                        assert(q.y > 10);
+                        assert(q.y < 100);
+                        assert(q.answer >= 0);
+                        console.log(q);
+                    }
+                });
             });
         });
-        context('when level3', () => {
-            it('xが10以上であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(3);
-                    assert(q.x >= 10);
-                }
-            });
-            it('yが10以上であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(3);
-                    assert(q.y >= 10);
-                }
-            });
-            it('答えが30以下であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(3);
-                    assert(q.answer <= 30);
-                }
-            });
+        context('multiplication', () => {
         });
-        context('when level4', () => {
-            it('xが10以上であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(4);
-                    assert(q.x >= 10);
-                }
-            });
-            it('yが10以上であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(4);
-                    assert(q.y >= 10);
-                }
-            });
-            it('答えが50以下であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(4);
-                    assert(q.answer <= 50);
-                }
-            });
-        });
-        context('when level5', () => {
-            it('xが10以上100未満であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(5);
-                    assert(q.x >= 10);
-                    assert(q.x < 100);
-                }
-            });
-            it('yが10以上100未満であること', () => {
-                for (let i = 0; i < 100; i++) {
-                    const q = Question.createQuestion(5);
-                    assert(q.y >= 10);
-                    assert(q.y < 100);
-                }
-            });
+        context('division', () => {
         });
     });
 });
